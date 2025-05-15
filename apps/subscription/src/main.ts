71e5e70eda5a17bed7f8904bd377ff279791acc5
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2025-05-15 19:30:21 +0300

    chore: configured microservice

--- apps/subscription/src/main.ts
+++ apps/subscription/src/main.ts
@@ -1,20 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'weather',
+      protoPath: 'libs/proto/src/weather.proto',
+      url: 'localhost:3000',
+    },
+  });
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen();
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Weather microservice is running on: http://localhost:${port}`,
   );
 }
 
