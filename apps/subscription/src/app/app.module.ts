06c610754fc844d19d92f8addf70c1613ee23c10
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2025-05-17 18:05:46 +0300

    chore: add modules to App module & config app

--- apps/subscription/src/app/app.module.ts
+++ apps/subscription/src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    SubscriptionModule,
+  ],
 })
 export class AppModule {}
--- apps/subscription/src/main.ts
+++ apps/subscription/src/main.ts
@@ -1,21 +1,26 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get<ConfigService>(ConfigService);
+
+  const port = configService.get<number>('port');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
-      package: 'weather',
-      protoPath: 'libs/proto/src/weather.proto',
-      url: 'localhost:3000',
+      package: 'subscription',
+      protoPath: '../../libs/proto/subscription.proto',
+      url: `127.0.0.1:${port}`,
     },
   });
-  const port = process.env.PORT || 3000;
   await app.listen();
   Logger.log(
-    `🚀 Weather microservice is running on: http://localhost:${port}`,
+    `🌧️ Weather microservice is running on: http://127.0.0.1:${port}`,
   );
 }
 
