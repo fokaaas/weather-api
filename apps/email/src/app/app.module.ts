61ed9ec6b239877a28d62137be91902e812618fb
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2025-05-18 09:06:54 +0300

    chore: config app & modules

--- apps/email/src/app/app.module.ts
+++ apps/email/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    EmailModule,
+  ],
 })
 export class AppModule {}
--- apps/email/src/main.ts
+++ apps/email/src/main.ts
@@ -1,20 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get<ConfigService>(ConfigService);
+
+  const port = configService.get<number>('port');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'email',
+      protoPath: 'libs/proto/email.proto',
+      url: `127.0.0.1:${port}`,
+    },
+  });
+  await app.listen();
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `📧 Email microservice is running on: http://127.0.0.1:${port}`,
   );
 }
 
